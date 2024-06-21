0c0d918fdd32006dbe542b97b57129b2af5ed556
sairevanth0007 <sairevanth0007@gmail.com>
2024-06-21 20:40:22 +0530

    added google analytics

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[https://smartmllabs.com/](smartmllabs) 
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-DP87C78RG3");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactGA.send({ hitType: "pageview", page: "/", title: "Views SMLL" });
+
 root.render(
   <React.StrictMode>
     <App />
